cf14ae4f93889b9b3b36b21bf219a47016f8fd3b
adam <3193391+bcuz@users.noreply.github.com>
2020-09-09 23:28:30 -0400

    vscode syntax bs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, { useState } from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-  const [home, setHome ] = useState(0);
-  const [away, setAway ] = useState(0);
+  const [home, setHome] = useState(0);
+  const [away, setAway] = useState(0);
 
   return (
     <div className="container">
@@ -31,12 +31,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={ () => setHome(home => home + 7) }>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={ () => setHome(home => home + 3) }>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => setHome(home => home + 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setHome(home => home + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={ () => setAway(away => away + 7) }>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={ () => setAway(away => away + 3) }>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setAway(away => away + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setAway(away => away + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
